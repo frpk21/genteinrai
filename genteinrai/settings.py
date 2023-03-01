625660cd34189297b99e45ddae807dc778fe1507
frpk21 <48810820+frpk21@users.noreply.github.com>
2023-03-01 10:35:06 -0500

    update

--- genteinrai/settings.py
+++ genteinrai/settings.py
@@ -163,7 +163,7 @@ LOGIN_REDIRECT_URL = '/'
 # cuando el usuario salga (logout), el programa lo manda al login.html
 LOGOUT_REDIRECT_URL = '/login/'
 
-try:
-    exec(open(os.path.join(BASE_DIR, 'genteinrai/settings_local.py')).read())
-except IOError:
-    raise Exception('error reading local settings')
+#try:
+#    exec(open(os.path.join(BASE_DIR, 'genteinrai/settings_local.py')).read())
+#except IOError:
+#    raise Exception('error reading local settings')
